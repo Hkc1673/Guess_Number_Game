f1d0d40d57ad1f441e8bca4ac17a4f24ca034447
habib <habibkc71@gmail.com>
2020-09-25 20:01:07 +0300

    remove team logo, add limit for guess attempt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let  btn  = document.getElementById("btn");
 let output = document.getElementById("outputtext");
 
-let number = [Math.floor(Math.random() * 101)];     // returns a random integer from 0 to 100
+let number = [Math.floor(Math.random() * 101)];     
 let counter = 0
 btn.addEventListener("click", function(){
     let input = document.getElementById("userinput").value;
@@ -9,6 +9,8 @@ btn.addEventListener("click", function(){
     if (input == number){
         output.innerHTML = `You guessed right, your number was: ${number}`
         document.getElementById("counter").innerHTML = "This is your test number:" + counter;
+        document.getElementById("userinput").value='';
+        counter = 0
     } else if (input < number){
         output.innerHTML = "You guessed too low!"
         document.getElementById("counter").innerHTML = "This is your test number:" + counter;
@@ -17,10 +19,10 @@ btn.addEventListener("click", function(){
         output.innerHTML = "You guessed too high!"
         document.getElementById("counter").innerHTML = "This is your test number:" + counter;
     }
-    if (counter == 5){
+    if (counter == 10){
         output.innerHTML = "Game Over! Try again..."
+        document.getElementById("userinput").value='';
         counter = 0
-        document.getElementById("counter").innerHTML = "This is your test number:" + counter;
     }
-    return
+
 });
